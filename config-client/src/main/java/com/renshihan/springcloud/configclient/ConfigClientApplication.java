73c6d550798da85e1e4879f5d55c822e96fed3a2
renshihan <379595488@qq.com>
2018-01-06 12:14:13 +0800

    config客户端

--- config-client/src/main/java/com/renshihan/springcloud/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/renshihan/springcloud/configclient/ConfigClientApplication.java
@@ -1,11 +1,18 @@
 package com.renshihan.springcloud.configclient;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class ConfigClientApplication {
 
+public class ConfigClientApplication {
+	@Autowired
+	void setEnviroment(Environment env) {
+		System.out.println("my-config.appName from env: "
+				+ env.getProperty("my-config.appName"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
