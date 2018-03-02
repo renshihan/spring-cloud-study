19b055a9db9b8d7747027df56313079cfc6dfc10
renshihan <379595488@qq.com>
2018-03-02 17:52:15 +0800

    config客户端

---
+++ spring-cloud-bus/src/main/java/com/renshihan/study/springcloudbus/SpringCloudBusApplication.java
@@ -0,0 +1,12 @@
+package com.renshihan.study.springcloudbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudBusApplication.class, args);
+	}
+}
--- spring-cloud-bus/src/test/java/com/renshihan/study/springcloudbus/SpringCloudBusApplicationTests.java
+++ spring-cloud-bus/src/test/java/com/renshihan/study/springcloudbus/SpringCloudBusApplicationTests.java
@@ -1,4 +1,4 @@
-package com.renshihan.springcloud.eureka;
+package com.renshihan.study.springcloudbus;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaServerApplicationTests {
+public class SpringCloudBusApplicationTests {
 
 	@Test
 	public void contextLoads() {
